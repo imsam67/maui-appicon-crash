7e570c5fca2163538251f9e49979753957712e85
imsam67 <sam@iQuestLLC.com>
2022-07-07 23:18:51 -0600

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			var result = await MediaPicker.PickPhotoAsync();
+			if (result == null)
+				return;
+
+			var fileName = result.FileName;
+			Debug.WriteLine("File picked: " + fileName);
+		}
+		catch(Exception ex)
+		{
+			// Log error
+			Debug.WriteLine("Error: " + ex.Message);
+		}
+	}
+}
+
